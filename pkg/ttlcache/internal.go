1fdc5fc03b8cd5c9c8d5ca023c5146211e24211c
Kyle Sanderson <kyle.leet@gmail.com>
2024-11-24 00:12:44 -0800

    theadsafety for gos

--- pkg/ttlcache/internal.go
+++ pkg/ttlcache/internal.go
@@ -5,8 +5,11 @@ import "time"
 func (c *Cache[K, V]) get(key K) (Item[V], bool) {
 	c.l.RLock()
 	defer c.l.RUnlock()
-	v, ok := c.m[key]
+	return c._g(key)
+}
 
+func (c *Cache[K, V]) _g(key K) (Item[V], bool) {
+	v, ok := c.m[key]
 	if !ok {
 		return v, ok
 	}
@@ -19,17 +22,28 @@ func (c *Cache[K, V]) set(key K, it Item[V]) Item[V] {
 
 	c.l.Lock()
 	defer c.l.Unlock()
+	return c._s(key, it)
+}
+
+func (c *Cache[K, V]) _s(key K, it Item[V]) Item[V] {
+	it.d, it.t = c.getDuration(it.d)
 	c.m[key] = it
 	c.ch <- it.t
 	return it
 }
 
 func (c *Cache[K, V]) getOrSet(key K, it Item[V]) (Item[V], bool) {
-	if v, ok := c.get(key); ok {
-		return v, ok
+	c.l.Lock()
+	defer c.l.Unlock()
+	return c._gos(key, it)
+}
+
+func (c *Cache[K, V]) _gos(key K, it Item[V]) (Item[V], bool) {
+	if g, ok := c._g(key); ok {
+		return g, ok
 	}
 
-	return c.set(key, it), true
+	return c._s(key, it), true
 }
 
 func (c *Cache[K, V]) delete(key K, reason DeallocationReason) {
